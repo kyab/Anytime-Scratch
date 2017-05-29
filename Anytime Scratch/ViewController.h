b2416b647d16b2ec2fc347f576f9c699764db8df
kyab <kyossi212@gmail.com>
2017-05-29 22:06:18 +0900

    Initial Commit

---
+++ Anytime Scratch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Anytime Scratch
+//
+//  Created by kyab on 2017/05/29.
+//  Copyright © 2017年 kyab. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ Anytime Scratch/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Anytime Scratch
+//
+//  Created by kyab on 2017/05/29.
+//  Copyright © 2017年 kyab. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
